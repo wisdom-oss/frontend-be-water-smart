4bb0d48238b72913fbe5728ce085254d42530871
Hushous <stueven.jan@web.de>
2024-10-14 17:29:09 +0200

    added translation file

---
+++ src/i18n/de_DE.ts
@@ -0,0 +1,22 @@
+export default {
+    "be-water-smart": {
+        titles: {
+            chart: "Vorhergesagter Wasserverbrauch",
+            model_select: "Modellauswahl für Vorhersage"
+        },
+        buttons: {
+
+        },
+        tables: {
+            model: {
+
+            },
+            smartmeter: {
+
+            },
+            virtualmeter: {
+
+            }
+        },
+    },
+};
---
+++ src/i18n/en_US.ts
@@ -0,0 +1,22 @@
+export default {
+    "be-water-smart": {
+        titles: {
+            chart: "Predicted Water Consumption",
+            model_select: "Model Selection for Prediction"
+        },
+        buttons: {
+
+        },
+        tables: {
+            model: {
+
+            },
+            smartmeter: {
+
+            },
+            virtualmeter: {
+
+            }
+        },
+    },
+};
